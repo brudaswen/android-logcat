59322456c5389e7618022b683d5074cca11acfdf
Sven Obser <sven@obser.info>
2025-10-05 16:03:41 +0200

    Add `main()` test code

---
+++ library/logcat-core/src/test/kotlin/de/brudaswen/android/logcat/core/Main.kt
@@ -0,0 +1,18 @@
+package de.brudaswen.android.logcat.core
+
+import de.brudaswen.android.logcat.core.parser.LogcatBinaryParser
+import kotlinx.coroutines.runBlocking
+import java.io.BufferedInputStream
+
+internal fun main() = runBlocking {
+    val process = ProcessBuilder("adb", "logcat", "-B").start()
+
+    LogcatBinaryParser(
+        input = BufferedInputStream(process.inputStream)
+    ).use { parser ->
+        while (true) {
+            val item = parser.parseItem()
+            println(item)
+        }
+    }
+}
